a23baf17e4e4e3a257c7070a36458d0e1410f731
hahan <hvha1@cmcglobal.vn>
2025-05-28 11:11:02 +0700

    ts loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,22 @@
 const path = require("path");
-// const toml = require('toml');
-// const yaml = require('yamljs');
-// const json5 = require('json5');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-      index: './src/index.js',
-      another: './src/another-module.js',
-      print: './src/print.js',
-  },
-  devtool: 'inline-source-map',
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true
-  },
-   optimization: {
-     splitChunks: {
-       chunks: 'all',
-     },
-   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.tsx$/i,
+        use: "ts-loader",
+        exclude: /node_module/,
       },
-      {
-        test: /.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      // {
-      //   test: /\.(csv|tsv)$/i,
-      //   use: ['csv-loader'],
-      // },
-      // {
-      //   test: /\.xml$/i,
-      //   use: ['xml-loader'],
-      // },
-      // {
-      //   test: /\.toml$/i,
-      //   type: 'json',
-      //   parser: {
-      //     parse: toml.parse,
-      //   },
-      // },
-      // {
-      //   test: /\.yaml$/i,
-      //   type: 'json',
-      //   parser: {
-      //     parse: yaml.parse,
-      //   },
-      // },
-      // {
-      //   test: /\.json5$/i,
-      //   type: 'json',
-      //   parser: {
-      //     parse: json5.parse,
-      //   },
-      // },
     ],
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
